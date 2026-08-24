f32f33e7251ce37392d744dfaf98d900d639c9e3
RoshanBhagtani2005 <bhagtani.roshan2005@gmail.com>
2026-08-25 01:18:37 +0530

    JS Update

--- JavaScript/04_Control_Flow/truthy.js
+++ JavaScript/04_Control_Flow/truthy.js
@@ -27,3 +27,24 @@ if (Object.keys(empobj).length === 0) {
     console.log("Object is empty");
 
 }
+
+//Nullish Coalescing Operator (??): null defined 
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null??10
+// val1 = undefined??15
+val1 = null??10??20
+
+console.log(val1);
+
+
+//ternary operator 
+
+// condition ?? true : false 
+
+const Iceteaprice = 100;
+
+Iceteaprice>=80 ?console.log("Greater then 80") : console.log("Less then 80");
+
+
