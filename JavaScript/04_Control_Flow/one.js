5c4598fd495b5bf33a9b5496032fad8a0057196b
RoshanBhagtani2005 <bhagtani.roshan2005@gmail.com>
2026-08-25 00:25:58 +0530

    JS Update

---
+++ JavaScript/04_Control_Flow/one.js
@@ -0,0 +1,50 @@
+const temperature = 41;
+
+if (temperature < 50) {
+    // console.log("less then 50");
+
+}
+else {
+    // console.log("greater then 50");
+
+}
+// console.log("Execute");
+
+// = , != ,<=,>=,===,!==
+
+const score = 200
+
+if (score > 100) {
+    const power = "fly"
+    // console.log(`Power is ${power}`);
+
+}
+// console.log(power);
+
+// this will give error because power is declared inside the if() it cannot be accessed outside the scope.
+
+const balance = 1000;
+
+// if(balance>500) console.log("true"),console.log("test case passed");
+
+if (balance < 500) {
+    console.log("Less then 500");
+
+}
+else if (balance < 750) {
+    console.log("less then 750");
+
+}
+else if (balance < 950) {
+    console.log("Less then 950");
+
+}
+
+else {
+    console.log("Less then 1200");
+
+}
+
+
+
+
